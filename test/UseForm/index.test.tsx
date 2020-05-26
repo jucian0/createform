0a26cee8caa78fe66408e97941d4fd3cdb0b7841
Juciano de Carvalho Barbosa <juciano.barbosa@sensedia.com>
2020-05-26 09:14:20 -0300

    test(useform): should change formState
    should change formState when change input value type number and should change formState when change
    input value type checkbox

--- test/UseForm/index.test.tsx
+++ test/UseForm/index.test.tsx
@@ -27,7 +27,7 @@ describe("UseForm hook ControlledForms forms", () => {
       })
    })
 
-   it("should change formState when change input value", () => {
+   it("should change formState when change input value type text", () => {
       const hookParams = {
          initialValues: { "test-name": "my-name-test" },
          onChange: true
@@ -42,5 +42,39 @@ describe("UseForm hook ControlledForms forms", () => {
       fireEvent.change(screen.getByTestId(inputParams.name), { target: { value: "new-name-test" } })
 
       expect(result.values).toEqual({ "test-name": "new-name-test" })
+   })
+
+   it("should change formState when change input value type checkbox", () => {
+      const hookParams = {
+         initialValues: { "test-accept": false },
+         onChange: true
+      }
+
+      const inputParams = {
+         name: "test-accept",
+         type: "checkbox"
+      }
+
+      const result = setup({ hookParams, inputParams })
+      fireEvent.click(screen.getByTestId(inputParams.name))
+
+      expect(result.values).toEqual({ "test-accept": true })
+   })
+
+   it("should change formState when change input value type number", () => {
+      const hookParams = {
+         initialValues: { "test-number": 22 },
+         onChange: true
+      }
+
+      const inputParams = {
+         name: "test-number",
+         type: "number"
+      }
+
+      const result = setup({ hookParams, inputParams })
+      fireEvent.change(screen.getByTestId(inputParams.name), { target: { value: 40 } })
+
+      expect(result.values).toEqual({ "test-number": 40 })
    })
 })
