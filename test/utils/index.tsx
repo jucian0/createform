b3fc109e72f6902fcccf0ed9cd98e2b3d5c97081
Juciano de Carvalho Barbosa <juciano.barbosa@sensedia.com>
2020-05-26 08:51:17 -0300

    test(useform): should change formState when change input value

---
+++ test/utils/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from '@testing-library/react';
+import { useForm } from '../../src/index'
+
+
+export function setup({ hookParams, inputParams }) {
+   const returnVal = {}
+
+   function InputComponent() {
+
+      const [{ values }, { input }] = useForm(hookParams)
+
+
+      Object.assign(returnVal, values)
+      return <input {...input(inputParams)} data-testid={inputParams.name} />
+   }
+   render(<InputComponent />)
+
+   return Object.assign({ values: returnVal }, { input: inputParams.name })
+}
