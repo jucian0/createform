4e812fe2f78191e13f2e7e01c077a10467720ef7
Juciano de Carvalho Barbosa <juciano.barbosa@sensedia.com>
2020-05-31 18:39:44 -0300

    test(useform/debounce): add tests useForm and deounce
    add tests useForm and debounce

--- test/utils/index.tsx
+++ test/utils/index.tsx
@@ -1,19 +1,50 @@
 import React from 'react'
 import { render } from '@testing-library/react';
 import { useForm } from '../../src/index'
+import Select from 'react-select'
 
-
-export function setup({ hookParams, inputParams }) {
-   const returnVal = {}
+export function setup({ hookParams, inputParams, onSubmit }: any) {
+   const returnVal: any = {}
 
    function InputComponent() {
 
-      const [{ values }, { input }] = useForm(hookParams)
+      const [{ values, ...rest }, { input }] = useForm(hookParams)
 
-      Object.assign(returnVal, values)
-      return <input {...input(inputParams)} data-testid={inputParams.name} />
+      Object.assign(returnVal, { values, ...rest })
+      return (
+         <form onSubmit={rest.onSubmit(onSubmit)} onReset={rest.reset}>
+            <input {...input(inputParams)} data-testid={inputParams.name} />
+            <button type="submit" data-testid="on-submit"></button>
+            <button type="reset" data-testid="on-reset"></button>
+         </form>
+      )
    }
    render(<InputComponent />)
 
-   return Object.assign({ values: returnVal }, { input: inputParams.name })
+   return Object.assign(returnVal, { input: inputParams.name })
+}
+
+export function customSetup({ hookParams, inputParams, onSubmit }: any) {
+   const returnVal: any = {}
+
+   function InputComponent() {
+
+      const [{ values, ...rest }, { custom }] = useForm(hookParams)
+
+      Object.assign(returnVal, { values, ...rest })
+      return (
+         <form onSubmit={rest.onSubmit(onSubmit)} onReset={rest.reset} role="form">
+            <Select
+               classNamePrefix="react-select"
+               options={inputParams.options}
+               {...custom(inputParams.name)}
+            />
+            <button type="submit" data-testid="on-submit"></button>
+            <button type="reset" data-testid="on-reset"></button>
+         </form>
+      )
+   }
+   render(<InputComponent />)
+
+   return Object.assign(returnVal, { input: inputParams.name })
 }
