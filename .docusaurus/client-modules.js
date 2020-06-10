14285b3ee204ecc40bb336f05df49cd73de10d70
Jucian0 <juciano@outlook.com.br>
2020-06-10 17:09:42 -0300

    chore(examples): add examples

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/barbosa/javascript/react-data-forms/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/barbosa/javascript/react-data-forms/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/barbosa/javascript/react-data-forms/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/barbosa/javascript/react-data-forms/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/barbosa/javascript/react-data-forms/src/css/custom.css"),
-];
