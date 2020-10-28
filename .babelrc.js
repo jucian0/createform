42137ef792f5f449150c1dbea8b2d9fc7d7b0314
Jucian0 <juciano@outlook.com.br>
2020-10-27 21:21:38 -0300

    add babel

---
+++ .babelrc.js
@@ -0,0 +1,12 @@
+module.export = {
+  env: {
+    test: {
+      plugins: [
+        'transform-es2015-modules-commonjs',
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-transform-runtime'
+      ],
+      presets: ['@babel/preset-react', '@babel/preset-typescript', '@babel/env']
+    }
+  }
+}
