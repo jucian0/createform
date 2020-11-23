c1c1eb0022965052d5fbfbe2fac3aea1f46ce6f3
Jucian0 <juciano@outlook.com.br>
2020-11-23 10:25:26 -0300

    chore(useformcontext): improve code

--- src/hooks/useContextForm.ts
+++ src/hooks/useContextForm.ts
@@ -4,10 +4,7 @@ import { FormContext } from "../core/contextForm";
 
 export function useFormContext<T>() {
 
-
    const form = React.useContext<UseFormReturnType<T>>(FormContext)
 
-
    return form
-
 }
