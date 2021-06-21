049fdc67ef0f1e3ab002f9a8f7ff6b9637a431e5
jucian0 <juciano@outlook.com.br>
2021-06-20 21:00:03 -0300

    fix(usefom): added throw erro when doesnt have a valid ref object
    fix #13

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -136,6 +136,11 @@ export function useForm<TO>({
    }
 
    function setRefValue(path: string, value: any) {
+      if (!refs.current[path]) {
+         throw new Error(
+            "You probably are trying to use a div as a Input component or your component doesn't have `forwardRef`"
+         )
+      }
       if (isCheckbox(refs.current[path].current.type)) {
          return (refs.current[path].current.checked = value)
       } else if (refs.current[path]?.current?.children) {
