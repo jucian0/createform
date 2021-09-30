f59ebeb35266d4ab0fab06d92d22bfb474264e2c
Jucian0 <juciano@outlook.com.br>
2021-09-30 20:59:52 -0300

    feat(formbuilder): add methods do deal with all kind of inputs

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,54 +1,13 @@
-class FormBuilder {
-   private state: FormState<any>
-
-   constructor(initialState: any) {
-      this.state = new FormState(initialState, onSubmit, onReset)
-   }
-
-   name(value: string) {
-      return this.state.setField('name', value)
-   }
-
-   email(value: string) {
-      return this.state.setField('email', value)
-   }
-
-   password(value: string) {
-      return this.state.setField('password', value)
-   }
-
-   onSubmit(onSubmitCallback: (state: any) => void) {
-      return this.state.onSubmit(onSubmitCallback)
-   }
-
-   onReset(onResetCallback: () => void) {
-      return this.state.onReset(onResetCallback)
-   }
-
-   getInitialState() {
-      return this.state.getInitialState()
-   }
-}
-
 const useForm = createForm((builder: any) => ({
    fields: {
-      name: builder.text('Name').required().validations(['required']).build(''),
-      email: builder
-         .email('Email')
-         .required()
-         .validations(['required'])
-         .build(''),
+      name: builder.text('Name').validations(['required']).build(''),
+      email: builder.email('Email').validations(['required']).build(''),
       password: builder
          .password('Password')
-         .required()
          .validations(['required'])
          .build(''),
       lastOne: {
-         path: builder
-            .text('LastOne')
-            .required()
-            .validations(['required'])
-            .build('')
+         path: builder.text('LastOne').validations(['required']).build('')
       }
    }
 }))
