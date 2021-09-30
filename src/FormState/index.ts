59f1ca451c866c8029103f76c9944cadca253eaf
Jucian0 <juciano@outlook.com.br>
2021-09-30 20:29:38 -0300

    feat(formstate): add a new state managemento to form

---
+++ src/FormState/index.ts
@@ -0,0 +1,51 @@
+export class FormState<T = any> extends ObservableForm<T> {
+   private initialState: T
+   constructor(
+      state: T,
+      private onSubmitCallback: (state: T) => void,
+      private onResetCallback: () => void
+   ) {
+      super(state)
+      this.initialState = state
+   }
+
+   setFields(values: T) {
+      this.set(values)
+      this.notify()
+   }
+
+   setField(field: string, value: any) {
+      this.patchState({ [field]: value } as Partial<T>)
+      this.notify()
+   }
+
+   resetFields() {
+      this.set(this.initialState)
+      this.notify()
+   }
+
+   resetField(field: string) {
+      this.patchState({ [field]: this.initialState[field] } as Partial<T>)
+      this.notify()
+   }
+
+   getFields() {
+      return this.get()
+   }
+
+   getField(field: string) {
+      return this.get()[field]
+   }
+
+   onSubmit() {
+      return this.onSubmitCallback(this.get())
+   }
+
+   onReset() {
+      this.onResetCallback()
+   }
+
+   getInitialState() {
+      return this.initialState
+   }
+}
