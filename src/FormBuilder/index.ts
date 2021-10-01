360f4938d95afb9039cb38e27e3280ed7581b58f
Jucian0 <juciano@outlook.com.br>
2021-10-01 09:36:49 -0300

    feat(formbuilder): remove unnecessary methos

--- src/FormBuilder/index.ts
+++ src/FormBuilder/index.ts
@@ -1,3 +1,40 @@
+type FormShape = {
+   [key: string]: any
+}
+
 export class FormBuilder {
-   constructor() {}
+   private values: FormShape
+   private errors: FormShape
+   private untouched: FormShape
+   private touched: FormShape
+   private pristine: FormShape
+   private dirty: FormShape
+
+   constructor(values: FormShape) {
+      this.values = values
+   }
+
+   public getValues(): FormShape {
+      return this.values
+   }
+
+   public getErrors(): FormShape {
+      return this.errors
+   }
+
+   public getUntouched(): FormShape {
+      return this.untouched
+   }
+
+   public getTouched(): FormShape {
+      return this.touched
+   }
+
+   public getPristine(): FormShape {
+      return this.pristine
+   }
+
+   public getDirty(): FormShape {
+      return this.dirty
+   }
 }
