fa01d56b0c9bac91f3987ef9e78fc6a7c3e28451
Jucian0 <juciano@outlook.com.br>
2021-10-30 21:42:41 -0300

    test(useform): add tests to radio type

--- __tests__/utils/makeSut.tsx
+++ __tests__/utils/makeSut.tsx
@@ -27,6 +27,46 @@ export function makeSut({ hookParams, inputParams, onSubmit }: any) {
    return { sut, hookState }
 }
 
+export function makeRadioSut({ hookParams, inputParams, onSubmit }: any) {
+   const hookState: UseFormReturnType<any> = Object.assign({})
+
+   function InputComponent() {
+      const { state, register, ...rest } = useForm<any>(hookParams)
+      Object.assign(hookState, { state, ...rest })
+
+      return (
+         <form onSubmit={rest.onSubmit(onSubmit)} onReset={rest.resetForm}>
+            <div {...register(inputParams.name)}>
+               <input
+                  type="radio"
+                  name={inputParams.name}
+                  value="option-1"
+                  data-testid="radio-1"
+               />
+               <input
+                  type="radio"
+                  name={inputParams.name}
+                  value="option-2"
+                  data-testid="radio-2"
+               />
+               <input
+                  type="radio"
+                  name={inputParams.name}
+                  value="option-3"
+                  data-testid="radio-3"
+               />
+            </div>
+            <button type="submit" data-testid="on-submit"></button>
+            <button type="reset" data-testid="on-reset"></button>
+         </form>
+      )
+   }
+
+   const sut = render(<InputComponent />)
+
+   return { sut, hookState }
+}
+
 export function makeUseFormParamsMock({
    value = '',
    name = 'inputName',
