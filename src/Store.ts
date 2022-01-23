f0b97d512b70c7d1a212b07a4e48280822988fef
Jucian0 <juciano@outlook.com.br>
2022-01-22 22:31:44 -0300

    feat(store): add Store to keep the form state

---
+++ src/Store.ts
@@ -0,0 +1,64 @@
+import * as Dot from './ObjectUtils'
+
+type Subscribe<TValues> = (e: TValues) => void
+type Subscribers<TValues = {}> = Array<Subscribe<TValues>>
+
+export function createStore<T extends {}>(initialState: T = Object.assign({})) {
+   let state = initialState
+   let subscribers: Subscribers<T> = []
+
+   function get(): T {
+      return state
+   }
+
+   function subscribe(fn: Subscribe<T>) {
+      subscribers = [...subscribers, fn]
+
+      return () => {
+         subscribers = subscribers.filter(l => l !== fn)
+      }
+   }
+
+   function set(nextState: T) {
+      state = nextState
+      notify()
+   }
+
+   function patch(path: string, next: any) {
+      const nextState = Dot.set(state, path, next)
+      if (typeof nextState !== 'undefined') {
+         state = nextState
+         notify()
+      } else {
+         throw new Error(`The path '${path}' is not defined`)
+      }
+   }
+
+   function getPropertyValue(path: string) {
+      return Dot.get(state, path)
+   }
+
+   function getInitialPropertyValue(path: string) {
+      return Dot.get(initialState, path)
+   }
+
+   function getInitialState(): T {
+      return initialState
+   }
+
+   function notify() {
+      subscribers.forEach(fn => {
+         fn(get())
+      })
+   }
+
+   return {
+      get,
+      set,
+      patch,
+      subscribe,
+      getPropertyValue,
+      getInitialState,
+      getInitialPropertyValue
+   }
+}
