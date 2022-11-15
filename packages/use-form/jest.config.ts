e69dc0a03044e1fae2a79ba6f4d615360b7c92af
jucian0 <juciano@outlook.com.br>
2022-11-15 18:29:31 -0300

    fix(use-form): fix Wrapper native envets types

---
+++ packages/use-form/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'useform',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/packages/useform',
+};
