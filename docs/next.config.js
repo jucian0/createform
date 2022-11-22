9219727d46314ebfdeb4c7294b79dcfa3bfc4e36
jucian0 <juciano@outlook.com.br>
2022-11-21 23:20:22 -0300

    docs(build): test new engrafia version

--- docs/next.config.js
+++
@@ -1,9 +0,0 @@
-const { engrafia } = require('engrafia/src/plugin');
-const withImages = require('next-images');
-const compose = require('compose-function');
-
-const composed = compose(engrafia, withImages);
-
-module.exports = composed({
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-});
