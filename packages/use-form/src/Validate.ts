e411a55679548127ca881f9d877a93506b8ce9b3
jucian0 <juciano@outlook.com.br>
2022-12-15 13:46:13 -0300

    fix(validation): fix validation function - convert function to async function

--- packages/use-form/src/Validate.ts
+++ packages/use-form/src/Validate.ts
@@ -1,23 +1,23 @@
-import { Schema, ValidationError } from 'yup'
-import * as Dot from './ObjectUtils'
+import { Schema, ValidationError } from 'yup';
+import * as Dot from './ObjectUtils';
 
 export function makeDotNotation(str: string) {
-   return str.split('[').join('.').split(']').join('')
+  return str.split('[').join('.').split(']').join('');
 }
 
-export function validate<TValues extends {}>(
-   values: TValues,
-   validationSchema: Schema<TValues>
+export async function validate<TValues extends {}>(
+  values: TValues,
+  validationSchema: Schema<TValues>
 ) {
-   return validationSchema
-      ?.validate(values, { abortEarly: false })
-      .then(() => {
-         return {}
-      })
-      .catch((e: ValidationError) => {
-         throw e.inner.reduce((acc, key) => {
-            const path = makeDotNotation(key.path)
-            return Dot.set(acc, path, key.message)
-         }, {})
-      })
+  return validationSchema
+    ?.validate(values, { abortEarly: false })
+    .then(() => {
+      return {};
+    })
+    .catch((e: ValidationError) => {
+      throw e.inner.reduce((acc, key) => {
+        const path = makeDotNotation(key.path);
+        return Dot.set(acc, path, key.message);
+      }, {});
+    });
 }
