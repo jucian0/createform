c214105c99f8331e899c7600784c0f0ffc694d3b
jucian0 <juciano@outlook.com.br>
2023-01-25 10:43:08 -0300

    fix(exception): remove unused codes

--- packages/use-form/src/Exception.ts
+++ packages/use-form/src/Exception.ts
@@ -1,19 +1,13 @@
 export class Exception extends Error {
-   constructor(message: string) {
-      super(message)
-      this.name = this.constructor.name
-      Error.captureStackTrace(this, this.constructor)
-   }
+  constructor(message: string) {
+    super(message);
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
 }
 
 export class InvalidArgumentException extends Exception {
-   constructor(message: string) {
-      super(message)
-   }
-}
-
-export class InvalidOperationException extends Exception {
-   constructor(message: string) {
-      super(message)
-   }
+  constructor(message: string) {
+    super(message);
+  }
 }
