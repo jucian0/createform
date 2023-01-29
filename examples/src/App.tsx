911f9effd5ae0d72c26a4da44358d686f541ba92
jucian0 <juciano@outlook.com.br>
2023-01-29 14:25:36 -0300

    feat(createform): adding inlineValidation for fields

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,12 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import 'react-datepicker/dist/react-datepicker.css';
+import { FormInlineValidation } from './Examples/InlineValidation';
 import { FormYupValidation } from './Examples/YupValidation';
 import { FormZodValidation } from './Examples/ZodValidation';
 
 export function App() {
   return (
     <ChakraProvider>
-      <FormZodValidation />
+      <FormInlineValidation />
     </ChakraProvider>
   );
 }
