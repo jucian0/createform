af1be620ceec2b5a0d20fd7899efa52b3e066fc5
jucian0 <juciano@outlook.com.br>
2023-02-09 17:43:39 -0300

    ci(github actions): replace yarn to pnpm

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,7 @@
-const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+  swcMinify: false,
 });
 
 module.exports = withNextra();
